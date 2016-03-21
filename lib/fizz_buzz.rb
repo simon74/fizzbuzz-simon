18c9ab2fad3f143ae7f2f144c631d104cc82291f
Raoul DIFFOUO <diraulo@gmail.com>
2016-03-21 13:30:35 +0200

    Add more tests cases

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,11 +1,11 @@
 def fizz_buzz(number)
-  if number == 3 
-  	return 'fizz'
-	elsif number == 5
-		return 'buzz'
-	elsif number == 15
-		return 'fizzbuzz'
-	else 
-		number
-	end
+  if number == 3
+    return 'fizz'
+  elsif number == 5
+    return 'buzz'
+  elsif number == 15
+    return 'fizzbuzz'
+  else
+    number
+  end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -14,15 +14,26 @@ describe 'fizz_buzz' do
   end
 
   it 'returns 4 when number is 4' do
-  	expect(fizz_buzz(4)).to eq 4
-  end
-	
-	it 'returns "buzz" when number is 5' do
-  	expect(fizz_buzz(5)).to eq 'buzz'
+    expect(fizz_buzz(4)).to eq 4
   end
 
-	it 'returns "fizzbuzz" when number is 15' do
-  	expect(fizz_buzz(15)).to eq 'fizzbuzz'
+  it 'returns "buzz" when number is 5' do
+    expect(fizz_buzz(5)).to eq 'buzz'
   end
 
+  it 'returns "fizzbuzz" when number is 15' do
+    expect(fizz_buzz(15)).to eq 'fizzbuzz'
+  end
+
+  it 'returns "fizz" when number is divisible by 3' do
+    expect(fizz_buzz(6)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when number is divisible by 5' do
+    expect(fizz_buzz(10)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when number is divisible by 15' do
+    expect(fizz_buzz(30)).to eq 'fizzbuzz'
+  end
 end
