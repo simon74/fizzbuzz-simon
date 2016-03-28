6308dd1e9d567c8c054e0c1abb1c6102d3f15ace
simon74 <simon@hemph.se>
2016-03-28 13:18:57 +0200

    changes and ex. with if/else and puts

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,11 +1,22 @@
+# puts ('write a number')
+	# number = Integer(gets)
+	# if number 
+	# 	fizz_buzz(number)
+	# end
+
 def fizz_buzz(number)
-  if number == 3
-    return 'fizz'
-  elsif number == 5
-    return 'buzz'
-  elsif number == 15
-    return 'fizzbuzz'
-  else
-    number
-  end
+	if number % 15 == 0
+	    # puts 'fizzbuzz'
+	    return 'fizzbuzz'
+	  elsif number % 3 == 0
+	    # puts 'fizz'
+	    return 'fizz'
+	  elsif number % 5 == 0
+	    #puts 'buzz'
+	    return 'buzz'
+	  else
+	    #puts "some text: #{number}"
+	    number
+	end
 end
+
