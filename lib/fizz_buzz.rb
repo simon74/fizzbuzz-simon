4d9daebd8eba90a99ae5f8b625e6da12c56e69dc
simon74 <simon@hemph.se>
2016-03-28 13:34:49 +0200

    Refactored

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,22 +1,24 @@
-# puts ('write a number')
-	# number = Integer(gets)
-	# if number 
-	# 	fizz_buzz(number)
-	# end
-
 def fizz_buzz(number)
-	if number % 15 == 0
-	    # puts 'fizzbuzz'
-	    return 'fizzbuzz'
-	  elsif number % 3 == 0
-	    # puts 'fizz'
-	    return 'fizz'
-	  elsif number % 5 == 0
-	    #puts 'buzz'
-	    return 'buzz'
-	  else
-	    #puts "some text: #{number}"
-	    number
-	end
+  case
+    when divisible_by_fifteen?(number) then 'fizzbuzz'
+    when divisible_by_five?(number) then 'buzz'
+    when divisible_by_three?(number) then 'fizz'
+    else number
+  end
 end
 
+def divisible_by_three?(number)
+  has_zero_reminder(number, 3)
+end
+
+def divisible_by_five?(number)
+  has_zero_reminder(number, 5)
+end
+
+def divisible_by_fifteen?(number)
+  has_zero_reminder(number, 15)
+end
+
+def has_zero_reminder(number, divider)
+  number % divider == 0
+end
